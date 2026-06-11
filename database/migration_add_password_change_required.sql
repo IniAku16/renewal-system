b83d9adb97d1de2ded9eaf40c7baa1f3a656bdd1
iniAku16 <ara.rhzz16@gmail.com>
2026-06-11 16:46:46 +0700

    11/06/2026

--- database/migration_add_password_change_required.sql
+++
@@ -1,7 +0,0 @@
--- Migration to add password_change_required column to users table
--- Run this query to update your database
-
-ALTER TABLE `users` ADD COLUMN `password_change_required` TINYINT(1) DEFAULT 0 AFTER `last_activity`;
-
--- Optional: Update existing users to require password change if needed
--- UPDATE `users` SET `password_change_required` = 1 WHERE `id_user` > 0;
